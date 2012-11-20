a4a6ddcf94bc216eb5a96566ebdb6e6756f045f1
Taro Kobayashi <9re.3000@gmail.com>
2012-11-20 23:46:50 +0900

    untabified

--- proj.android/src/com/teionclub/test/Cocos2DTest.java
+++ proj.android/src/com/teionclub/test/Cocos2DTest.java
@@ -28,12 +28,11 @@ import org.cocos2dx.lib.Cocos2dxActivity;
 import android.os.Bundle;
 
 public class Cocos2DTest extends Cocos2dxActivity{
-
-	protected void onCreate(Bundle savedInstanceState){
-		super.onCreate(savedInstanceState);
-	}
-	
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
+    
     static {
-         System.loadLibrary("game");
+	System.loadLibrary("game");
     }
 }
